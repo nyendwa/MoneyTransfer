b8ec3ff763da08963ec3e2fcdbe4cc3f35817f4b
nyendwa <nyendwa@yahoo.com>
2024-03-05 01:03:39 +0200

    commit first project

---
+++ login.php
@@ -0,0 +1,51 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Swift</title>
+</head>
+
+<body>
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">SWIFT</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+
+                    <li><a href="#" class="business">BUSINESS</a></li>
+                    <li><a href="#" class="customer">CUSTOMER</a></li>
+                    <li><a href="#" class="login">LOG IN</a></li>
+                    <li><a href="#" class="register">REGISTER</a></li>
+                </ul>
+
+            </div>
+            <div class="search">
+                <input type="search" class="srch" placeholder="Type to search...">
+                <a href="#"><button class="btn">Search</button></a>
+            </div>
+        </div>
+
+        <div class="content">
+            <div>
+                <h1>Make your money move</h1>
+                <p class="slogan">
+                    Pay online, send money and earn loyalty points with a<br> digital wallet used by millions.
+                </p>
+                <p>
+                    <button class="Signup"><a href="">Sign up now!</a></button>
+                    <button class="Learnmore"><a href="">Learn more</a></button>
+                </p>
+
+            </div>
+        </div>
+
+
+    </div>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,35 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="register.css">
+</head>
+
+<body>
+    <div class="backgroundcolor">
+        <div class="form">
+            <h2 class="reg_here">Register Here</h2>
+            <p>Already have an account? <a href="">Log in</a></p>
+            <input name="firstname" type="text" placeholder="First Name">
+            <input name="lastname" type="text" placeholder="Last Name">
+            <div>
+                <select id="dropdown" name="Country">
+                    <option value="" disabled selected>Country</option>
+                    <option value="apple">Apple</option>
+                    <option value="banana">Banana</option>
+                    <option value="orange">Orange</option>
+                    <option value="grape">Grape</option>
+                    <!-- Add more options as needed -->
+                </select>
+            </div>
+            <input name="currency" type="text" placeholder="Currency">
+            <input placeholder="Enter email here" name="email" type="email">
+            <input name="" type="password" placeholder="Password">
+
+        </div>
+    </div>
+</body>
+
+</html>
